82cead5ec6e335642724d10f34e9aeec8c9105b9
Philippe Babin <philippe.babin@gmail.com>
2013-01-15 22:43:09 -0500

    Add easing effect with Bezier Curve and targeting

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var pause = false;
+
 var framerate = 60;
 
 var canvas = document.getElementById('canvas');
@@ -8,64 +8,76 @@ ctx.translate(0, 0);
 
 var W = canvas.width, H = canvas.height;
 var o = new Vector(W/2, H/2, 0);
+var zero = new Vector(0,0,0);
 var mouse = new Vector(0, 0, 0);
 var mouseInit = new Vector();
+var pause = false;
+var s = 10;
 
 var cam = new Vector(0, 0, 10);
+var camL = new Vector(1,0,0);
+var camU = new Vector(0,1,0);
+var camD = new Vector(0,0,1);
+
 var fov = 70;
 var n = 0.1;
 var far = 5;
-var s = 10;
 
-var camL = new Vector(1,0,0);
-var camU = new Vector(0,1,0);
 camL = camL.unit();
 camU = camU.unit();
-var camD = new Vector(0,0,1);
 
 
 var camAngle = 0;
 var camAngleY = 0;
 
 var poly = Array();
+var gunOriginal = Array();
 var gun = Array();
 var zBuffer;
 
 var perspectiveM = new Matrix();
 
-var pOn = true;
-var cOn = true;
 var fOn = true;
 var velocity = new Vector(0,0,0);
 var jLock = false;
 
+var loadingModel = 0;
+var nbrModel = 0;
 var vectex = Array();
 var polygon = Array();
 
+var tran = 0;
+var is_up = false;
+var tLock = false;
+var timerT;
 
-function loadModel( url, pArray, mult){
-  var allText;
-  var txtFile = new XMLHttpRequest();
-  txtFile.open("GET", url, true);
-  txtFile.onreadystatechange = function() {
-    if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
-      if (txtFile.status === 200) {  // Makes sure it's found the file.
-        loadObj(txtFile.responseText, pArray, mult);
+
+function loadModel( url, pArray, mult, cb){
+    loadingModel++;
+    nbrModel++;
+  var mdFile = new XMLHttpRequest();
+  mdFile.open("GET", url, true);
+  mdFile.onreadystatechange = function() {
+    if (mdFile.readyState === 4) {  
+      if (mdFile.status === 200) {
+        loadObj(mdFile.responseText, pArray, mult);
+        loadingModel--;
+        cb();
       }
     }
   };
-  txtFile.send(null);
+  mdFile.send(null);
 }
 
 
 
-function loadObj( txt, pArray, mult) {
+function loadObj( file, pArray, mult) {
     vectex = Array();
     polygon = Array();
     var g, fArray;
-    txt = txt.split("\n");
-    for(g = 0; g < txt.length; g++){
-      handleData(txt[g]);
+    file = file.split("\n");
+    for(g = 0; g < file.length; g++){
+      handleData(file[g]);
     }
             
      var v = Array();
@@ -114,56 +126,13 @@ function handleData( line){
         
 }
 
-function handleFileSelect(evt) {
-    vectex = Array();
-    polygon = Array();
-    var txt = "", out = "",g;
-    var files = evt.target.files; // FileList object
-
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-
-      var reader = new FileReader();
-
-      // Closure to capture the file information.
-      reader.onload = (function(theFile) {
-        return function(e) {
-        
-        txt = e.target.result.split("\n");
-         for(var g = 0; g < txt.length; g++){
-             handleData(txt[g]);
-         }
-            
-     var v = Array();
-     for(g = 0; g < vectex.length; g++){
-         v[g] = new Vector( vectex[g][0], vectex[g][1], vectex[g][2]);
-     }
-        var fArray;    
-     for(g = 0; g < polygon.length; g++){
-         fArray = Array();
-         for(var p = 0; p < polygon[g].length; p++){
-            fArray.push(v[polygon[g][p]]);
-         }
-         poly.push(new Poly(fArray));
-     }
-     };
-     })(f);
-
-      reader.readAsText(f);
-    }
-    
-    
- }
-
 function Poly(vArray){
   this.P = Array();
   for(var q = 0; q < vArray.length; q++){
     this.P[q]= vArray[q];
   }
 }
-//function Poly(vp){
-//  this.P = vp;
-//}
+
 function add3V(va, vb, vc){
   var pa = Array();
   pa[0] = va;
@@ -241,13 +210,6 @@ function createCylinder( from, r, length, n){
   
 }
 
-function lengthToCam( po){
-  var re = 0;
-  for(var q = 0; q < po.P.length; q++){
-    re += po.P[q].subtract(cam).length();
-  }
-  return re / po.P.length;
-}
 function vecRot(v, a){
   var x2, y2, z2, x3, y3, z3, x4, y4, z4;
   x2 = v.x * Math.cos(a.z) - v.y * Math.sin(a.z);  
@@ -264,17 +226,36 @@ function vecRot(v, a){
   
   return new Vector(x4, y4, z4);
 }
-function polyRotation(po, angle){
-  var np = new Poly( vecRot(po.P[0], angle), vecRot(po.P[1], angle), vecRot(po.P[2], angle));
-  return np;
+function polyRot(po, angle){
+  var pa = Array();
+  for(var q = 0; q < po.P.length; q++){
+    pa[q]= vecRot(po.P[q], angle);
+  }
+  
+  return new Poly(pa);
 }
-function drawPoly(p){
+function modelRot(m, angle){
+  for(var q = 0; q < m.length; q++){
+    m[q]= polyRot(m[q], angle);
+  }
+}
+function modelTrans(m, t){
+  for(var q = 0; q < m.length; q++){
+    for(var j = 0; j < m[q].P.length; j++){
+        m[q].P[j] = m[q].P[j].add(t);
+    }
+  }
+}
+function drawPoly(p, is_view){
   var V = Array();
   var Vz = Array();
   var drawed = true;
   
   for(var q = 0; q < p.P.length; q++){
-    V[q] = doView(p.P[q]);
+    if(is_view)
+        V[q] = doView(p.P[q]);
+    else
+        V[q] = p.P[q];
     Vz[q] = V[q].z;
     if(Vz[q] < 0) 
       drawed = false;
@@ -315,20 +296,26 @@ function drawVector3f( g, d){
     ctx.stroke();
 }
 
-function drawModel( pArray){
+function lengthToCam(cP, po){
+  var re = 0;
+  for(var q = 0; q < po.P.length; q++){
+    re += po.P[q].subtract(cP).length();
+  }
+  return re / po.P.length;
+}
+function drawModel( pArray, is_view){
   zBuffer = Array();
   for(var q = 0; q < pArray.length; q++){
-    var foo = Array(2);
-    
-    foo[0] = q;
-    foo[1] = lengthToCam(pArray[q]);
-    zBuffer.push(foo);
+    if(is_view)
+        zBuffer.push([q,lengthToCam(cam,pArray[q])]);
+    else
+        zBuffer.push([q,lengthToCam(zero,pArray[q])]);
   }
   
   zBuffer.sort(sorting);
   
   for(q = 0; q < zBuffer.length; q++){
-    drawPoly(pArray[zBuffer[q][0]]);
+    drawPoly(pArray[zBuffer[q][0]], is_view);
   }
 }
 
@@ -345,16 +332,35 @@ function drawGod(){
 
 function draw(){
   
-  ctx.fillStyle = "white"; 
+    ctx.fillStyle = "white"; 
+      
+    ctx.fillRect (0, 0, W, H);
+    ctx.fillStyle = "blue";
+      
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = "1";
+    ctx.fillStyle = "black";
   
-  ctx.fillRect (0, 0, W, H);
-  ctx.fillStyle = "blue";
-  
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = "1";
-  
-  drawModel(poly);
-  drawModel(gun);
+  if(loadingModel === 0){
+//        if(tran > 20)
+//            is_up = true;
+//        if(tran < -40)
+//            is_up = false;
+//        if(is_up){
+//            tran --;
+//            modelRot(gun, new Vector(0,Math.PI/256,0));
+//        }
+//        else{
+//            tran ++;
+//            modelRot(gun, new Vector(0,-Math.PI/256,0));
+//        }
+        
+        drawModel(poly,true);
+        drawModel(gun,false);
+        //drawModel(gunOriginal,false);
+  }
+  else
+     ctx.fillText("Model loading ["+(nbrModel-loadingModel)+"/"+nbrModel+"]", W/2-50, H/2);
   drawGod();
   
   if(pause) 
@@ -367,6 +373,8 @@ function draw(){
   ctx.fillText("Mouse - x:"+mouse.x+" y:"+mouse.y, 4, H-70);
   ctx.fillText("S = "+s, 4,18);
   ctx.fillText("Polygons = "+poly.length, 4,32);
+  ctx.fillText("T= "+tran, 4,46);
+  ctx.fillText("xT= "+(new Date().getTime() - timerT)/1000, 4,66);
  // ctx.fillText("CamL - x:"+(((camL.x*1000)>>0)/1000)+" y:"+camL.y+" z:"+(((camL.z*1000)>>0)/1000), 4,40);
 //  ctx.fillText("CamD - x:"+(((camD.x*1000)>>0)/1000)+" y:"+camD.y+" z:"+(((camD.z*1000)>>0)/1000), 4,60);
 //  ctx.fillText("CamU - x:"+(((camU.x*1000)>>0)/1000)+" y:"+camU.y+" z:"+(((camU.z*1000)>>0)/1000), 4,80);
@@ -381,7 +389,47 @@ function draw(){
 //  var uc = dc.cross(lc).unit().negative().multiply(100);
 }
 
+function bezierFunc(p2, p3, t) {
+   var a = t;
+   var b = 1 - t;
+    var p4 = 1;
+      
+   return 3 * p2 * Math.pow(b, 2) * a + 3 * p3 * b * Math.pow(a, 2) + p4 * Math.pow(a, 3);
+}
 function update(){
+  
+  
+    if(loadingModel === 0){
+        gun = gunOriginal.slice(0);
+        //var xT = 0.5;
+        var xT = (new Date().getTime() - timerT)/500;
+        if(is_up && !tLock){
+           // tran = 1000/(new Date().getTime() - timerT);
+            //tran = bezierFunc(0.5, 0.5, 1-xT);
+            tran = bezierFunc(-0.55, 1.26, 1-xT);
+            tran = bezierFunc(1, 1, 1-xT);
+        }
+        else if(!is_up && tLock){
+            //tran = 1- 100/(new Date().getTime() - timerT);
+            //tran = bezierFunc(0.5, 0.5, xT);.68,-0.55,.16,1.26
+            tran = bezierFunc(-0.55, 1.26, xT);
+            tran = bezierFunc(1, 1, xT);
+        }
+        if(xT >= 1 && tLock){
+            tran = 1;
+            is_up = true;
+        }
+        else if(xT >= 1 && !tLock){
+            tran = 0;
+            is_up = false;
+        }
+        //modelRot(gun, new Vector(Math.PI/2,Math.PI/200 * tran,0)); 
+        modelRot(gun, new Vector(Math.PI/2,0,0)); 
+        //new Vector(-0.1,-2.1,3) new Vector(-5,-5,10)
+        //modelTrans(gun, new Vector(-0.1,-2.1,3));
+        modelTrans(gun, new Vector(-0.1,-2.1,3).multiply(tran).add((new Vector(-5,-5,10)).multiply(1-tran)));
+    }
+  
   
   velocity.y -= 0.1;
   //cam = cam.add(velocity);
@@ -462,8 +510,21 @@ function init(){
   
   
   
-  loadModel("ressources/ingenieur.obj", poly, 0.1);
-  loadModel("ressources/gun.obj", gun, 1);
+  loadModel("ressources/gun.obj", gunOriginal, 1, function(){
+            gun = gunOriginal.slice(0);
+            if(true){
+                modelRot(gun, new Vector(Math.PI/2,0,0));
+                modelTrans(gun, new Vector(-0.1,-2.1,3));
+            }
+            else{
+                modelRot(gunOriginal, new Vector(Math.PI/2,0,0));
+                modelTrans(gunOriginal, new Vector(-5,-5,10));
+            }
+        });
+//  loadModel("ressources/ingenieur.obj", poly, 0.1, function(){
+//            //modelTrans(poly, new Vector(10,10,-10));
+//        });
+        
 //  loadModel("http://jsbin.com/uvanat/2", poly, 0.1);
 //  loadModel("http://jsbin.com/uvanat/6", gun, 1);
   
@@ -537,10 +598,10 @@ function doKeyDown(evt){
         jump();
     break;
     case 88: //x
-        pOn = !pOn;
+        tLock = !tLock;
+        timerT = new Date().getTime();
     break;
     case 90:  //z
-        cOn = !cOn;
     break;
   }
 }
@@ -555,7 +616,6 @@ function doKeyPress(evt){
 }
 window.addEventListener('keydown',doKeyDown,true);
 window.addEventListener('keypress',doKeyPress,true);
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
 
 //Code from : https://developer.mozilla.org/en-US/docs/API/Pointer_Lock_API#Browser_compatibility
